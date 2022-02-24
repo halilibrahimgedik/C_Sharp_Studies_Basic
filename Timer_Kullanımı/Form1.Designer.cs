e627f39ce7c8c30776db902f03d57452fb9c64c0
Halil İbrahim Gedik <100279727+halilgedik4234@users.noreply.github.com>
2022-02-24 23:56:47 +0300

    Timer Kullanımı 1

---
+++ Timer_Kullanımı/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Timer_Kullanımı
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 444);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Timer_Kullanımı/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer_Kullanımı
+{
+    public partial class form1 : System.Windows.Forms.Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+        int sayac=0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            sayac++;
+            label1.Text = sayac.ToString();
+            if (sayac == 5)
+            {
+                this.BackColor = Color.LightBlue;
+            }
+            else if (sayac == 10) { this.BackColor = Color.LightGreen; }
+            else if (sayac == 15) { this.BackColor = Color.LightGreen; }
+            else if (sayac == 20) { this.BackColor = Color.Blue; }
+            else if (sayac == 25) { this.BackColor = Color.Yellow; }
+            else if (sayac == 30) { this.BackColor = Color.PeachPuff; }
+            if (sayac==30)
+            {
+                sayac=0;
+            }
+        }
+    }
+}
