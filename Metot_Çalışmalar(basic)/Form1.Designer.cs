436d7df38e71715b6dd703b8395d1738a9282ab4
Halil İbrahim Gedik <100279727+halilgedik4234@users.noreply.github.com>
2022-03-02 08:42:10 +0300

    Calculation of 2 numbers avarage with basic methods

---
+++ Metot_Çalışmalar(basic)/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Metot_Çalışmalar_basiz_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxToplam = new System.Windows.Forms.TextBox();
+            this.txtBoxÇarpım = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 33);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 33);
+            this.label2.TabIndex = 1;
+            // 
+            // txtBoxToplam
+            // 
+            this.txtBoxToplam.Location = new System.Drawing.Point(180, 53);
+            this.txtBoxToplam.Name = "txtBoxToplam";
+            this.txtBoxToplam.Size = new System.Drawing.Size(171, 40);
+            this.txtBoxToplam.TabIndex = 2;
+            // 
+            // txtBoxÇarpım
+            // 
+            this.txtBoxÇarpım.Location = new System.Drawing.Point(180, 122);
+            this.txtBoxÇarpım.Name = "txtBoxÇarpım";
+            this.txtBoxÇarpım.Size = new System.Drawing.Size(171, 40);
+            this.txtBoxÇarpım.TabIndex = 3;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(193, 194);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(131, 47);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 33);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "1.Sayı:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 33);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "2.Sayı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 337);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtBoxÇarpım);
+            this.Controls.Add(this.txtBoxToplam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxToplam;
+        private System.Windows.Forms.TextBox txtBoxÇarpım;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Metot_Çalışmalar(basic)/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metot_Çalışmalar_basiz_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        int _Toplam(int s1, int s2)
+        {
+         return s1+ s2;
+        }
+
+        int _Çarpım(int s1, int s2)
+        {
+            return s1 * s2;
+        }
+
+        int _Bölüm(int s1, int s2)
+        {
+            return s1 / s2;
+        }
+
+        int _Çıkarma (int s1, int s2)
+        {
+            return s1 - s2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int s=Convert.ToInt32(txtBoxToplam.Text);
+            int y= Convert.ToInt32(txtBoxÇarpım.Text);
+            label1.Text = "Toplamları: " + _Toplam(s,y).ToString();
+            label2.Text= "Çarpımları: "+ _Çarpım(s,y).ToString();
+        }
+    }
+}
