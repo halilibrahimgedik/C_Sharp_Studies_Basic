f8d30c68752af8838ccc54a82f26f19e42e266b9
Halil İbrahim Gedik <100279727+halilgedik4234@users.noreply.github.com>
2022-03-03 15:25:30 +0300

    studying with classes

---
+++ Classlar/Araba.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar
+{
+
+    public class Insan
+    {
+        int Kiilo;
+        string Ad;
+        double Boy;
+        int Yas;
+        public void Yazdır(int Kilo,string Ad,double Boy,int Yas)
+        {
+            this.Ad = Ad;
+            this.Boy = Boy;
+            this.Yas = Yas;
+            this.Kiilo = Kilo;
+            Console.WriteLine(this.Kiilo);
+            Console.WriteLine(this.Ad);
+            Console.WriteLine(this.Boy);
+            Console.WriteLine(this.Yas);
+        }
+        //overloading
+        public void Yazdır(int kilooo)
+        {
+            Kiilo= kilooo;
+            Console.WriteLine(Kiilo);
+        }
+
+    }
+    public class deneme
+    {
+        int Kilo;
+        public int kilo
+        {
+            get { return Kilo; }
+            set
+            {
+                if (value < 0)
+                {
+                    Kilo = 0;
+                }
+                else
+                Kilo = value; 
+                
+            }
+        }
+    }
+
+    class Kisiler
+    {
+        public void kisilistesi()
+        {
+      
+          
+            Console.WriteLine("Ad Giriniz: ");
+            string ad=Console.ReadLine();
+            Console.Write("Tekrar sayısını giriniz: ");
+
+            int x=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine(ad);
+            }
+           
+        }
+    }
+
+
+}
---
+++ Classlar/islem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar
+{
+    internal class islem
+    {
+
+        public int _Topla (int s1,int s2)
+        {
+         
+            int s3= s1 + s2;
+            Console.WriteLine("sonuc: "+ s3);
+            return s3;
+        }
+        public int _Cıkart(int s1, int s2)
+        {
+            int s3 = s1 - s2;
+            Console.WriteLine("sonuc: " + s3);
+            return s3;
+        }
+        public int _Böl(int s1, int s2)
+        {
+            int s3 = s1 / s2;
+            Console.WriteLine("sonuc: " + s3);
+            return s3;
+        }
+        public int _Carp(int s1, int s2)
+        {
+            int s3 = s1 * s2;
+            Console.WriteLine("sonuc: " + s3);
+            return s3;
+        }
+    }
+}
