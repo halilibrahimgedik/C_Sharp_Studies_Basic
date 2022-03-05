20393dd0ea1636428b2daf69ab8bf09cb2f4023d
Halil İbrahim Gedik <100279727+halilgedik4234@users.noreply.github.com>
2022-03-05 17:10:55 +0300

    reading Txt file

---
+++ MetinBelgesiOkuma/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MetinBelgesiOkuma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                string satir= sr.ReadLine();
+                while (satir != null)
+                {
+                    listBox1.Items.Add(satir);
+                    satir = sr.ReadLine();
+                }
+               
+            }
+        }
+    }
+}
