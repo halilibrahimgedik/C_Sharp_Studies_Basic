309ea74d3b26c1ddf692cf14526f2a4d365be53d
Halil İbrahim Gedik <100279727+halilgedik4234@users.noreply.github.com>
2022-03-10 22:03:04 +0300

    how to use MenuStrip

---
+++ MenuStripKullanımı/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace MenuStripKullanımı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aksiyonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aksiyonToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fantasticToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerçekOlaylarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hızlıErişimToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.theSocialNetworkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.theTomorrowWarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.marvelİnfinityWarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spiderManNoWayHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inceptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ınterstellarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kırmızıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maviToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.açıkYeşilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sarıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkımdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aksiyonToolStripMenuItem,
+            this.haToolStripMenuItem,
+            this.hızlıErişimToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(854, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aksiyonToolStripMenuItem
+            // 
+            this.aksiyonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aksiyonToolStripMenuItem1,
+            this.fantasticToolStripMenuItem,
+            this.gerçekOlaylarToolStripMenuItem});
+            this.aksiyonToolStripMenuItem.Name = "aksiyonToolStripMenuItem";
+            this.aksiyonToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.aksiyonToolStripMenuItem.Text = "Filmler";
+            // 
+            // aksiyonToolStripMenuItem1
+            // 
+            this.aksiyonToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.theTomorrowWarToolStripMenuItem,
+            this.inceptionToolStripMenuItem});
+            this.aksiyonToolStripMenuItem1.Name = "aksiyonToolStripMenuItem1";
+            this.aksiyonToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.aksiyonToolStripMenuItem1.Text = "Aksiyon";
+            // 
+            // fantasticToolStripMenuItem
+            // 
+            this.fantasticToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.marvelİnfinityWarToolStripMenuItem,
+            this.spiderManNoWayHomeToolStripMenuItem});
+            this.fantasticToolStripMenuItem.Name = "fantasticToolStripMenuItem";
+            this.fantasticToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fantasticToolStripMenuItem.Text = "Fantastic";
+            // 
+            // gerçekOlaylarToolStripMenuItem
+            // 
+            this.gerçekOlaylarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.theSocialNetworkToolStripMenuItem,
+            this.ınterstellarToolStripMenuItem});
+            this.gerçekOlaylarToolStripMenuItem.Name = "gerçekOlaylarToolStripMenuItem";
+            this.gerçekOlaylarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gerçekOlaylarToolStripMenuItem.Text = "Dram / Uyarlama";
+            // 
+            // haToolStripMenuItem
+            // 
+            this.haToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kırmızıToolStripMenuItem,
+            this.maviToolStripMenuItem,
+            this.açıkYeşilToolStripMenuItem,
+            this.sarıToolStripMenuItem});
+            this.haToolStripMenuItem.Name = "haToolStripMenuItem";
+            this.haToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.haToolStripMenuItem.Text = "Renkler";
+            // 
+            // hızlıErişimToolStripMenuItem
+            // 
+            this.hızlıErişimToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hakkımdaToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.hızlıErişimToolStripMenuItem.Name = "hızlıErişimToolStripMenuItem";
+            this.hızlıErişimToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.hızlıErişimToolStripMenuItem.Text = "Hızlı Erişim";
+            // 
+            // theSocialNetworkToolStripMenuItem
+            // 
+            this.theSocialNetworkToolStripMenuItem.Name = "theSocialNetworkToolStripMenuItem";
+            this.theSocialNetworkToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.theSocialNetworkToolStripMenuItem.Text = "The Social Network";
+            this.theSocialNetworkToolStripMenuItem.Click += new System.EventHandler(this.theSocialNetworkToolStripMenuItem_Click);
+            // 
+            // theTomorrowWarToolStripMenuItem
+            // 
+            this.theTomorrowWarToolStripMenuItem.Name = "theTomorrowWarToolStripMenuItem";
+            this.theTomorrowWarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.theTomorrowWarToolStripMenuItem.Text = "The Tomorrow War";
+            this.theTomorrowWarToolStripMenuItem.Click += new System.EventHandler(this.theTomorrowWarToolStripMenuItem_Click);
+            // 
+            // marvelİnfinityWarToolStripMenuItem
+            // 
+            this.marvelİnfinityWarToolStripMenuItem.Name = "marvelİnfinityWarToolStripMenuItem";
+            this.marvelİnfinityWarToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.marvelİnfinityWarToolStripMenuItem.Text = " Infinity War";
+            this.marvelİnfinityWarToolStripMenuItem.Click += new System.EventHandler(this.marvelİnfinityWarToolStripMenuItem_Click);
+            // 
+            // spiderManNoWayHomeToolStripMenuItem
+            // 
+            this.spiderManNoWayHomeToolStripMenuItem.Name = "spiderManNoWayHomeToolStripMenuItem";
+            this.spiderManNoWayHomeToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.spiderManNoWayHomeToolStripMenuItem.Text = "Spider-Man No Way Home";
+            this.spiderManNoWayHomeToolStripMenuItem.Click += new System.EventHandler(this.spiderManNoWayHomeToolStripMenuItem_Click);
+            // 
+            // inceptionToolStripMenuItem
+            // 
+            this.inceptionToolStripMenuItem.Name = "inceptionToolStripMenuItem";
+            this.inceptionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inceptionToolStripMenuItem.Text = "İnception";
+            this.inceptionToolStripMenuItem.Click += new System.EventHandler(this.inceptionToolStripMenuItem_Click);
+            // 
+            // ınterstellarToolStripMenuItem
+            // 
+            this.ınterstellarToolStripMenuItem.Name = "ınterstellarToolStripMenuItem";
+            this.ınterstellarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ınterstellarToolStripMenuItem.Text = "Interstellar";
+            this.ınterstellarToolStripMenuItem.Click += new System.EventHandler(this.ınterstellarToolStripMenuItem_Click);
+            // 
+            // kırmızıToolStripMenuItem
+            // 
+            this.kırmızıToolStripMenuItem.Name = "kırmızıToolStripMenuItem";
+            this.kırmızıToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kırmızıToolStripMenuItem.Text = "Kırmızı";
+            this.kırmızıToolStripMenuItem.Click += new System.EventHandler(this.kırmızıToolStripMenuItem_Click);
+            // 
+            // maviToolStripMenuItem
+            // 
+            this.maviToolStripMenuItem.Name = "maviToolStripMenuItem";
+            this.maviToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.maviToolStripMenuItem.Text = "Mavi";
+            this.maviToolStripMenuItem.Click += new System.EventHandler(this.maviToolStripMenuItem_Click);
+            // 
+            // açıkYeşilToolStripMenuItem
+            // 
+            this.açıkYeşilToolStripMenuItem.Name = "açıkYeşilToolStripMenuItem";
+            this.açıkYeşilToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.açıkYeşilToolStripMenuItem.Text = " Yeşil";
+            this.açıkYeşilToolStripMenuItem.Click += new System.EventHandler(this.açıkYeşilToolStripMenuItem_Click);
+            // 
+            // sarıToolStripMenuItem
+            // 
+            this.sarıToolStripMenuItem.Name = "sarıToolStripMenuItem";
+            this.sarıToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sarıToolStripMenuItem.Text = "Sarı";
+            this.sarıToolStripMenuItem.Click += new System.EventHandler(this.sarıToolStripMenuItem_Click);
+            // 
+            // hakkımdaToolStripMenuItem
+            // 
+            this.hakkımdaToolStripMenuItem.Name = "hakkımdaToolStripMenuItem";
+            this.hakkımdaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hakkımdaToolStripMenuItem.Text = "Hakkımda";
+            this.hakkımdaToolStripMenuItem.Click += new System.EventHandler(this.hakkımdaToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(21, 78);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(803, 417);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(352, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Fragman Projesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aksiyonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aksiyonToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fantasticToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerçekOlaylarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem theTomorrowWarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inceptionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem marvelİnfinityWarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spiderManNoWayHomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem theSocialNetworkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ınterstellarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kırmızıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maviToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem açıkYeşilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sarıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hızlıErişimToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkımdaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MenuStripKullanımı/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuStripKullanımı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kırmızıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor=Color.Red;
+        }
+
+        private void maviToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.CadetBlue;
+        }
+
+        private void açıkYeşilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void sarıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+        private void hakkımdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu çalışma Halil İbrahim Gedik tarafından yapılmıştır...","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void theTomorrowWarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/rQMQvvJssWs");
+            label1.Text = "Yarınının Sınırında";
+        }
+
+        private void inceptionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/vgNN9sk2MWw");
+            label1.Text = "Inception";
+        }
+
+        private void marvelİnfinityWarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/NWepvH6LnEw");
+            label1.Text = "Sonsuzluk Savaşı";
+        }
+
+        private void spiderManNoWayHomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/mQ_F_0jA59k");
+            label1.Text = "Spider-Man Eve Dönüş Yok";
+        }
+
+        private void theSocialNetworkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/q3mSOWEM5bY");
+            label1.Text = "The Social Network";
+        }
+
+        private void ınterstellarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://youtu.be/vVJeYMRam0o");
+            label1.Text = "Yıldızlar Arasında";
+        }
+    }
+}
